3ba418786690474f2deceba720bc57fd0a37747d
reyhanacar <reyhanozkanacar@gmail.com>
2023-05-03 00:56:09 +0300

    Reyhan Acar- Project is completed.

---
+++ src/test/java/Test/Reyhan/StepDefs/Hooks.java
@@ -0,0 +1,13 @@
+package Test.Reyhan.StepDefs;
+
+import Utilities.Driver;
+import io.cucumber.java.After;
+
+public class Hooks {
+
+    @After(order = 0)
+    public void after(){
+        Driver.quitDriver();
+    }
+
+}
