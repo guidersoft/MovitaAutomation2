76599866b2fd4bf370092ea838313266461f7292
ademgencer <ademyasargencer@gmail.com>
2023-07-02 23:20:43 +0300

    Arac Bazlı Rapor alt menuye ait default gorunum tamamlandı. 02.07.2023

--- src/test/java/Test/Adem/stepdefs/Hooks_Mvt.java
+++ src/test/java/Test/Adem/stepdefs/Hooks_Mvt.java
@@ -7,6 +7,8 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import static Test.Adem.stepdefs.Base_Mvt.softAssert;
+
 public class Hooks_Mvt {// Cucumber notasyonları, varsa TestNG notasyonlarından sonra çalışırlar.
 
     @After(order = 2)// Her senaryo sonrası çalışır.
@@ -14,11 +16,12 @@ public class Hooks_Mvt {// Cucumber notasyonları, varsa TestNG notasyonlarında
         if (!scenario.getSourceTagNames().contains("@Raporlar")) {// Bu tagli senaryo haricinde her senaryo bitimi quit eder.
             Driver.quitDriver();
         }
-
+        //@After("not @Raporlar") şeklinde yazarak da Raporlar taglı senaryonun çalışması engellenebilir.
     }
 
 
-    @After()// After her senaryo sonrası çalışır. -> AfterStep dersek her step sonu, AfterAll dersek tüm senaryoların bitiminde çalışır.
+    @After()// After her senaryo sonrası çalışır. -> AfterStep dersek her step sonu,
+    // AfterAll dersek tüm senaryoların bitiminde çalışır.
     public void afterScreenShot(Scenario scenario) {
         boolean screenShot = PropertyReader.read().get("takescreenshot").equalsIgnoreCase("true");// true -> Bu senaryoda SS alınacak.
         boolean screenShotOn = PropertyReader.read().get("takescreenshot.on").equalsIgnoreCase("false");// false -> Ama her stepte SS alınmasın.
@@ -26,7 +29,7 @@ public class Hooks_Mvt {// Cucumber notasyonları, varsa TestNG notasyonlarında
             if (screenShotOn) {// step step SS alınacaksa bu bloğa girer -> tabi önce AfterStep diyerek bu metotu step step çalıştırmalıyız.
                 if (scenario.isFailed()) {// Senaryo fail ise bu bloğa girer.
                     byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-                    // ekran fotosunu file değilde byte olarak(0 ve 1 ler gibi sayısal olark) tutar.
+                    // ekran fotosunu file değilde byte olarak(0 ve 1 ler gibi sayısal olarak) tutar.
                     scenario.attach(screenshot, "image/png", scenario.getName());
                     // senaryomuza attach(ek) yapıyoruz; screenshot, media tip ve senaryo adını giriyoruz.
                     // Bu ekran fotosunu raporda görebileceğiz.
@@ -39,9 +42,9 @@ public class Hooks_Mvt {// Cucumber notasyonları, varsa TestNG notasyonlarında
         }
     }
 
-    @After(value = "@LoginPageGorunum", order = 1)// Sadece bu tagli senaryo sonrası çalışır.
+    @After(value = "@LoginPageGorunum or @AracBazliRapor", order = 1)// Sadece bu tagli senaryoda(senaryo sonrası) çalışır.
     public void softAssertAll(Scenario scenario) {
-        MyStepdefs_Mvt.softAssert.assertAll("Senaryo : " + scenario.getName());
+        softAssert.assertAll("Senaryo : " + scenario.getName());
     }
 
 }
